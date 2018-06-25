8eb334ac08d414150cecafc5b077b6b680c20d1a
Carlo Blohm <carlo.blohm@solarisBank.de>
2018-06-25 12:44:52 +0200

    do not leak

--- collector/exporter.go
+++ collector/exporter.go
@@ -68,7 +68,11 @@ type mysqlHealth struct {
 
 var mH mysqlHealth
 
-
+func init() {
+	// use a channel to be thread safe
+	mH.writerch = make(chan bool)
+	go mH.set()
+}
 
 // New returns a new MySQL exporter for the provided DSN.
 func New(dsn string, scrapers []Scraper) *Exporter {
@@ -85,10 +89,6 @@ func New(dsn string, scrapers []Scraper) *Exporter {
 		dsn = dsn + "?"
 	}
 	dsn += strings.Join(dsnParams, "&")
-	// use a channel to be thread safe
-	mH.writerch = make(chan bool)
-	go mH.set()
-	defer close(mH.writerch)
 
 	return &Exporter{
 		dsn:      dsn,
